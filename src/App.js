19f2fddfc3c06c11b6ed19d0507ad6abcefde712
= <maria.louisa.castro@gmail.com>
2021-12-10 12:24:52 -0600

    Added search bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ function App() {
   return (
     <div className="App">
       <h1>Dictionary</h1>
+      <input type="text"></input>
+      <button type="submit">SEARCH</button>
     </div>
   );
 }
